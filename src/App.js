c3b49b7b3ffca36ad9161ca714ed0c0ed323920d
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-03-16 00:56:17 +0600

    feat: add task#3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import Avatar from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div class="wrapper">
+        <div class="resume_block">
+            <div class="profile_data">
+                <img src={Avatar} class="profile_data_img" alt="avatar"></img>
+                <div class='profile_data_name_block'>
+                  <p class="profile_data_name" title="Shrek Ogrbekuly Ogrsinabek Akenin Akesi Papanin Papasynyn balasy">Shrek Ogrbekuly Ogrsinabek Akenin Akesi Papanin Papasynyn balasy</p>
+                  <span>@shrek</span>
+                </div>
+            </div>
+            <p class="quote">
+                Всем привет, я живу на болоте!<br></br>
+                Вот мой адрес: <a target="_blank" rel="noopener noreferrer" href="https://2gis.kz/nur_sultan">
+                  https://2gis.kz/nur_sultan
+                  </a>
+            </p>
+            <div class="hints">
+                <div class="time_block">
+                    <span>11:32 •</span>
+                    <span>2 марта 2022г</span>
+                </div>
+                <div class="like_block">
+                    <span>Нравится: 255 •</span>
+                    <span>Комментарии: 7 •</span>
+                    <span>Репоcт: 3</span>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
